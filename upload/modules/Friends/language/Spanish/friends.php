3b9b31399834c7d0e97b7a7ba480eb61981fcaee
zJerino <48019027+zJerino@users.noreply.github.com>
2020-05-29 16:02:13 -0400

    Create friends.php

---
+++ upload/modules/Friends/language/Spanish/friends.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ *	FRIENDS MODULE
+ *	By Xemah | https://xemah.com
+ *
+**/
+
+$language_version = '2.0.0-pr7';
+
+$language = array(
+    
+    // General Terms
+	'friends' => 'Amigos',
+	'no_friends' => 'Sin amigos	.',
+	'add_friend' => 'Agregar amigos',
+	'remove_friend' => 'Eliminar amigo',
+	'accept_friend_request' => 'Aceptar solicitud de amistad',
+	'cancel_friend_request' => 'Cancelar solicitud de amistad',
+	
+    // Alert Terms
+	'received_friend_request' => '{x} te ha enviado una solicitud de amistad.',
+	'accepted_friend_request' => '{x} ha aceptado tu solicitud de amistad.',
+	
+    // Flash Terms
+	'friend_removed' => 'Se ah eliminado a {x} de su lista de amigos.',
+	'friend_request_sent' => 'Se envió con éxito la solicitud de amistad a {x}.',
+	'friend_request_accepted' => 'Aceptó con éxito la solicitud de amistad de {x}.',
+	'friend_request_canceled' => 'Se canceló con éxito la solicitud de amistad para {x}.',
+);
