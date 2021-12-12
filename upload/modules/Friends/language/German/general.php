67344b96293e650f07ffe7d137482610a30e63e3
Enrico <41052284+enno123@users.noreply.github.com>
2021-12-12 13:03:11 +0100

    German translation added
    German translation added

---
+++ upload/modules/Friends/language/German/general.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *	FRIENDS MODULE
+ *	By Xemah | https://xemah.com translated by enno123
+ *
+**/
+
+$language_version = '2.0.0-pr7';
+
+$language = [
+    
+	'title' => 'Freunde',
+	'friends' => 'Freunde',
+	'no_friends' => 'Keine Freunde.',
+	'add_friend' => 'Freund hinzufügen',
+	'remove_friend' => 'Freund entfernen',
+	'accept_friend_request' => 'Freundschaftsanfrage annehmen',
+	'cancel_friend_request' => 'Freundschaftsanfrage ablehnen',
+	
+	'received_friend_request' => '{x} hat dir eine Freundschaftsanfrage geschickt.',
+	'accepted_friend_request' => '{x} hat deine Freundschaftsanfrage angenommen.',
+	
+	'friend_removed' => 'Du hast {x} erfolgreich von deiner Freundesliste entfernt.',
+	'friend_request_sent' => 'Erfolgreich eine Freundschaftsanfrage an {x} gesendet.',
+	'friend_request_accepted' => 'Die Freundschaftsanfrage von {x} wurde erfolgreich angenommen.',
+	'friend_request_canceled' => 'Die Freundschaftsanfrage an {x} wurde erfolgreich gelöscht.',
+	
+];
