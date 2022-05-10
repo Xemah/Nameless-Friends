08ccc3a8de3cd2eefd7c0ef5cc5cfaaebb359b98
Justman10000 <justmegaliga10000@gmail.com>
2022-05-10 22:26:47 +0200

    German added

---
+++ upload/modules/Friends/language/German/general.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ *	FRIENDS MODULE
+ *	By Xemah | https://xemah.com
+ *
+**/
+
+$language_version = '2.0.0-pr7';
+
+$language = [
+    
+	'title' => 'Freunde',
+	'friends' => 'Freunde',
+	'no_friends' => 'Keine Freunde.',
+	'add_friend' => 'Freund hinzufügen',
+	'remove_friend' => 'Freund entfernen',
+	'accept_friend_request' => 'Freundschaftsanfrage akzeptieren',
+	'cancel_friend_request' => 'Freundschaftsanfrage ablehnen',
+	
+	'received_friend_request' => '{x} hat dir eine Freundschaftsanfrage gesendet.',
+	'accepted_friend_request' => '{x} hat deine Freundschaftsanfrage akzeptiert.',
+	
+	'friend_removed' => 'Erfolgreich {x} Freunde von deiner Freundesliste entfermt.',
+	'friend_request_sent' => 'Erfolgreich eine Freundschaftsanfrage an {x} gesendet.',
+	'friend_request_accepted' => 'Freundschaftsanfrage von {x} erfolgreich akzeptiert.',
+	'friend_request_canceled' => 'Freundschaftsanfrage an {x} erfolgreich zurückgezogen.',
+	
+];
