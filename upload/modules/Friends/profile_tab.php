caf7436c030f7e0676cbe4d83f841cb885e18f5d
PadowYT2 <71085027+PadowYT2@users.noreply.github.com>
2022-06-10 09:05:49 +0300

    pr13

--- upload/modules/Friends/profile_tab.php
+++ upload/modules/Friends/profile_tab.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- *	FRIENDS MODULE
- *	By Xemah | https://xemah.com
+ *    FRIENDS MODULE
+ *    By Xemah | https://xemah.com
  *
 **/
 
 $profileUser = explode('/', trim($_GET['route'], '/'));
 $profileUser = $profileUser[1];
-$profileUser = $queries->getWhere('users', ['username', '=', $profileUser]);
+$profileUser = DB::getInstance()->get('users', ['username', '=', $profileUser])->results();
 $profileUser = $profileUser[0];
 
 require(__DIR__ . '/classes/Friends.php');
@@ -18,25 +18,25 @@ $friends->processPost();
 $friendsList = [];
 $friendsQuery = $friends->query($profileUser->id);
 if (!empty($friendsQuery)) {
-	foreach ($friendsQuery as $friend) {
-		$friendID = (($friend->user_id == $profileUser->id) ? $friend->friend_id : $friend->user_id);
-		$friendUser = new User($friendID);
-		$friendsList[] = [
-			'id' => $friendUser->data()->id,
-			'uuid' => $friendUser->data()->uuid,
-			'avatar' => $friendUser->getAvatar(),
-			'profile' => $friendUser->getProfileURL(),
-			'username' => $friendUser->getDisplayname(true),
-			'nickname' => $friendUser->getDisplayname(),
-			'style' => $friendUser->getGroupClass(),
-			'title' => Output::getClean($friendUser->data()->user_title),
-		];
-	}
+    foreach ($friendsQuery as $friend) {
+        $friendID = (($friend->user_id == $profileUser->id) ? $friend->friend_id : $friend->user_id);
+        $friendUser = new User($friendID);
+        $friendsList[] = [
+            'id' => $friendUser->data()->id,
+            'uuid' => $friendUser->data()->uuid,
+            'avatar' => $friendUser->getAvatar(),
+            'profile' => $friendUser->getProfileURL(),
+            'username' => $friendUser->getDisplayname(true),
+            'nickname' => $friendUser->getDisplayname(),
+            'style' => $friendUser->getGroupClass(),
+            'title' => Output::getClean($friendUser->data()->user_title),
+        ];
+    }
 }
 
 $smarty->assign('FRIENDS', [
-	'title' => $friendsLanguage->get('general', 'title'),
-	'button' => $friends->generateButton(),
-	'list' => $friendsList,
-	'no_friends' => $friendsLanguage->get('general', 'no_friends'),
+    'title' => $friendsLanguage->get('general', 'title'),
+    'button' => $friends->generateButton(),
+    'list' => $friendsList,
+    'no_friends' => $friendsLanguage->get('general', 'no_friends'),
 ]);
